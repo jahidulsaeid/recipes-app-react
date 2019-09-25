5460dc412675baa8333badc83ee18d4769e99b44
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-09-25 17:23:19 +0600

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export class About extends Component {
   render() {
     return (
-      <div>
+      <div className="container mt-5">
+        <h1>This is food2fork recipes site</h1>
+        <p>version 0.1</p>
         
       </div>
     )
